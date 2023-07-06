4844477618591a1c62c9e916470a47445286296f
zhaominyue <zhaominyue@bytedance.com>
2023-07-06 16:38:45 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {action, observable} from "mobx";
+import {TimerView} from "./components/TimerView";
 
 function App() {
+  const appState = observable({
+    timer: 0
+  });
+  appState.resetTimer = action(function reset() {
+    appState.timer = 0;
+  });
+  setInterval(action(function tick() {
+    appState.timer += 1;
+  }), 1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TimerView appState={appState} />
     </div>
   );
 }
